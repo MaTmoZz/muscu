36d57e602f388c9e6f588547c12944976d91b1e7
Matthieu Pelissier <95907052+MaTmoZz@users.noreply.github.com>
2026-07-04 20:45:19 +0200

    Add Sandwich Protéiné to food base

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ with tab2:
 
         # Base d'aliments (1 portion)
         foods = {
+            "Sandwich Protéiné": {"kcal": 501, "prot": 26.92, "gluc": 55.2, "lip": 18.04},
             "Œuf entier (1 pièce ~60g)": {"kcal": 90, "prot": 7.2, "gluc": 0.4, "lip": 6.6},
             "Whey (1 scoop 30 g)":      {"kcal": 114.6, "prot": 23.4, "gluc": 2.69,   "lip": 1.11},
             "Gainer (1 scoop 50 g)":      {"kcal": 192.5, "prot": 12.5, "gluc": 33.5,   "lip": 0.75},
